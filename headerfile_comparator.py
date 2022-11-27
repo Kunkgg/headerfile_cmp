bcf8be5a7770c83b9507e4ebaa8ba98f14278735
gk07 <gk_520@hotmail.com>
2022-11-27 20:06:05 +0800

    micro refactor, rename

--- headerfile_comparator.py
+++ headerfile_comparator.py
@@ -1,8 +1,9 @@
 import difflib
 import filecmp
 import logging
+from dataclasses import dataclass
 from functools import cached_property
-from typing import Dict
+from typing import Dict, List
 
 import common.init_log
 from common.utils import readlines
@@ -10,8 +11,14 @@ from common.utils import readlines
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class DiffItem:
+    name: str
+    diff_html: str
+
+
 class HeaderFileComparator:
-    def __init__(self, from_fn, to_fn, from_desc='from', to_desc='to'):
+    def __init__(self, from_fn, to_fn, from_desc="from", to_desc="to"):
         self.from_fn = from_fn
         self.to_fn = to_fn
         self.from_desc = from_desc
@@ -30,25 +37,51 @@ class HeaderFileComparator:
     def to_lines(self):
         return readlines(self.to_fn)
 
-    def text_cmp(self) -> Dict:
+    def make_from_desc(self, desc_parts: List[str]) -> str:
+        return "/".join([self.from_desc] + desc_parts)
+
+    def make_to_desc(self, desc_parts: List[str]) -> str:
+        return "/".join([self.to_desc] + desc_parts)
+
+    def cmp_text(self) -> Dict:
+        text_from_desc = self.make_from_desc([self.from_fn])
+        text_to_desc = self.make_to_desc([self.to_fn])
+
         text_diff = (
             ""
             if self.is_same
             else self.differ.make_table(
-                self.from_lines, self.to_lines, self.from_desc, self.to_desc
+                self.from_lines, self.to_lines, text_from_desc, text_to_desc
             )
         )
 
         return {"is_same": self.is_same, "text_diff": text_diff}
 
-    def define_cmp(self):
+    def cmp_define(self):
+        # res = {
+        #     "diff_count": 5,
+        #     "from_only": ["std_only_defin_test1", "std_only_defin_test2"],
+        #     "to_only": ["dev_only_defin_test1", "dev_only_defin_test2"],
+        #     "diffs": [{"name": "define_diff_test1", "diff_html": ""}],
+        # }
+
+        res = {
+            "diff_count": 0,
+            "from_only": [],
+            "to_only": [],
+            "diffs": [],
+        }
+        if self.is_same:
+            return res
+
+    def cmp_enum(self):
         pass
 
-    def enum_cmp(self):
+    def cmp_variable(self):
         pass
 
-    def variable_cmp(self):
+    def cmp_struct(self):
         pass
 
-    def struct_cmp(self):
+    def cmp_include(self):
         pass
--- tests/test_headerfile_comparator.py
+++ tests/test_headerfile_comparator.py
@@ -7,18 +7,18 @@ class TestHeaderFileComparator(unittest.TestCase):
         fn = "./tests/fixtures/ast.h"
         fn_copy = "./tests/fixtures/ast_copy.h"
         fn_modified = "./tests/fixtures/ast_modified.h"
-        cmp_copy = HeaderFileComparator(fn, fn_copy)
-        cmp_modified = HeaderFileComparator(fn, fn_modified)
-        self.assertTrue(cmp_copy.is_same)
-        self.assertFalse(cmp_modified.is_same)
+        cmptor_copy = HeaderFileComparator(fn, fn_copy)
+        cmptor_modified = HeaderFileComparator(fn, fn_modified)
+        self.assertTrue(cmptor_copy.is_same)
+        self.assertFalse(cmptor_modified.is_same)
 
     def test_text_cmp(self):
         fn = "./tests/fixtures/ast.h"
         fn_copy = "./tests/fixtures/ast_copy.h"
         fn_modified = "./tests/fixtures/ast_modified.h"
-        cmp_copy = HeaderFileComparator(fn, fn_copy)
-        cmp_modified = HeaderFileComparator(fn, fn_modified)
-        cmp_copy_text_diff = cmp_copy.text_cmp().get('text_diff')
-        cmp_modified_text_diff = cmp_modified.text_cmp().get('text_diff')
-        self.assertEqual(cmp_copy_text_diff, "")
-        self.assertNotEqual(cmp_modified_text_diff, "")
+        cmptor_copy = HeaderFileComparator(fn, fn_copy)
+        cmptor_modified = HeaderFileComparator(fn, fn_modified)
+        cmptor_copy_text_diff = cmptor_copy.cmp_text().get('text_diff')
+        cmptor_modified_text_diff = cmptor_modified.cmp_text().get('text_diff')
+        self.assertEqual(cmptor_copy_text_diff, "")
+        self.assertNotEqual(cmptor_modified_text_diff, "")
